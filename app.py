5d30f2d6f52ed080ae0799faafa093b6c7ff52e9
CharlesKibet <charlesbiegon973@gmail.com>
2024-12-30 20:00:03 +0300

    Adding the dependencies and initializing them

--- app.py
+++ app.py
@@ -2,13 +2,42 @@
 #Importing the necessary libraries
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_bcrypt  import Bcrypt
+from flask_jwt_extended import JWTManager
+
+#Importing the db object
+from models import db
 
 #Importing our endpoint
 from resources.hello import HelloResource
 
 app = Flask(__name__)
 
+#Initializing the API
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teeflex.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+
+#Binding the app to the db object
+db.init_app(app)
+
+#Enabling CORS
+CORS(app)
+
+#Migrating the database
+migrate = Migrate(app, db)
+
+#Initiliazing the bcrypt object
+bcrypt = Bcrypt(app)
+
+#Initializing the JWTManager object
+jwt = JWTManager(app)
 
 #Creating an API object
 api.add_resource(HelloResource, '/')
--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 # Modelling the the database schema
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+
+# To ensure consistency in the naming of the constraints, we can define a naming convention that will be used by SQLAlchemy.
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+# We then pass the naming convention to the MetaData object
+metadata = MetaData(naming_convention=convention)
+
+# We then create an instance of the SQLAlchemy class and pass the metadata object to it
+db = SQLAlchemy(metadata=metadata)
