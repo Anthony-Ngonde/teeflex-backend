c3dd0771f6c490bd02e84ca33ef70e90b547647a
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-06 15:38:04 +0300

    Defining the models

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
-#Designing the GUI for the application
-#Importing the necessary libraries
+# Designing the GUI for the application
+# Importing the necessary libraries
 import os
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_bcrypt  import Bcrypt
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-#Importing the db object
+# Importing the db object
 from models import db
 
-#Importing our endpoint
+# Importing our endpoint
 from resources.hello import HelloResource
 
 app = Flask(__name__)
 
-#Initializing the API
+# Initializing the API
 api = Api(app)
 
-#Configuring the app
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SUPABASE_URL')
+# Configuring the app
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SUPABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teeflex.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('JWT_SECRET')
 
 
-#Binding the app to the db object
+# Binding the app to the db object
 db.init_app(app)
 
-#Enabling CORS
+# Enabling CORS
 CORS(app)
 
-#Migrating the database
+# Migrating the database
 migrate = Migrate(app, db)
 
-#Initiliazing the bcrypt object
+# Initiliazing the bcrypt object
 bcrypt = Bcrypt(app)
 
-#Initializing the JWTManager object
+# Initializing the JWTManager object
 jwt = JWTManager(app)
 
-#Creating an API object
+# Creating an API object
 api.add_resource(HelloResource, '/')
-
-
--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 
 # To ensure consistency in the naming of the constraints, we can define a naming convention that will be used by SQLAlchemy.
@@ -18,3 +19,28 @@ metadata = MetaData(naming_convention=convention)
 
 # We then create an instance of the SQLAlchemy class and pass the metadata object to it
 db = SQLAlchemy(metadata=metadata)
+
+
+class Member(db.Model, SerializerMixin):
+
+    # Defining the table
+    __tablename__ = 'members'
+
+    # Defining the columns in our database
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(80), nullable=False)
+    l_name = db.Column(db.String(80), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+
+
+class Payment(db.Model, SerializerMixin):
+
+    # Defining the table
+    __tablename__ = 'payments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    plan = db.Column(db.String, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime)
