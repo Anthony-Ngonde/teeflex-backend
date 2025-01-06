f3d1427511dd382e8429b60aadee2634662c28dd
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-06 19:41:48 +0300

    Updating resources in our database now working

--- resources/paymentdetails.py
+++ resources/paymentdetails.py
@@ -1,5 +1,6 @@
 # Endpoint for performing CRUD operations on payments
 from flask_restful import Resource, reqparse
+from sqlalchemy import and_, not_
 from models import db, Payment, Member
 from datetime import datetime
 
@@ -73,3 +74,42 @@ class PaymentResource(Resource):
             if payment == None:
                 return {'message': 'Payment not found'}
             return payment.to_dict()
+
+    def patch(self, id):
+        # Endpoint to update the payments
+
+        data = self.parser.parse_args()
+
+        payment = Payment.query.filter_by(id=id).first()
+
+        if not payment:
+            return {'message': "No payment to update"}
+        else:
+            payment_date = datetime.strptime(
+                data['date'], '%Y-%m-%d')
+
+        # Checking to confirm the number we are trying to update is there in our database
+        phone_number = Member.query.filter_by(
+            phone_number=data['phone_number']).first()
+
+        if phone_number == None:
+            return {'message': "Phone number not found"}
+
+        # Checking if the transaction id already is there and belongs to a certain member
+        transaction_id = db.session.query(Payment).filter(
+            and_(Payment.transaction_id == data['transaction_id'],
+                 not_(Payment.id == id))
+        ).first()
+
+        if transaction_id:
+            return {"message": 'Transaction id is already in use'}
+
+        payment.phone_number = data['phone_number']
+        payment.transaction_id = data['transaction_id']
+        payment.plan = data['plan']
+        payment.amount = data['amount']
+        payment.date = payment_date
+
+        db.session.commit()
+
+        return {'message': "Payment details updated successfully"}
