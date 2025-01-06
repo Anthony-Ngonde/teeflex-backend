fe15c34ca1d4835bd696e34338af16ec68afef8f
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-06 20:54:14 +0300

    Seeding the database wroking for both payement and members

--- seed.py
+++ seed.py
@@ -18,3 +18,14 @@ with app.app_context():
     db.session.commit()
 
     print('New member added')
+
+    print('Seeding new payment')
+    
+    new_payment = Payment(phone_number='0758431218',
+                          transaction_id='075934122', plan='monthly',
+                          amount=500,
+                          date=datetime.strptime('2024-05-01','%Y-%m-%d'), member_id=member.id)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    print('Added new payment')
