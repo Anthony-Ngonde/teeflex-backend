cba976406174991265ac899fc57e0d68577ac298
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-07 22:42:10 +0300

    Notifications for payments working

--- resources/notifications.py
+++ resources/notifications.py
@@ -1,11 +1,28 @@
-#Endpoint for handling our notifications
+# Endpoint for handling our notifications
 from flask_restful import Resource
-from models import Notification
+from models import db, Notification
+
 
 class NotificationResource(Resource):
 
-    #This will be just support get methods
+    # This will be just support get methods
     def get(self):
-        notifications = Notification.query.order_by(Notification.created_at.desc()).all()
-        
+        notifications = Notification.query.order_by(
+            Notification.created_at.desc()).all()
+
         return [notification.to_dict() for notification in notifications]
+
+
+class MarkNotificationReadResource(Resource):
+
+    # This will be the endpoint to mark our notifications as read
+    def patch(self, id):
+        notification = Notification.query.get(id)
+        if not notification:
+            return {'message': "No recent updates"}, 404
+
+        notification.is_read = True
+
+        db.session.commit()
+
+        return {'message': 'Notification marked as read'}
